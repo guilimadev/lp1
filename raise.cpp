73be55c475f98ea787ad64cd7b240a09a06fa49b
Guilherme de Lima Menezes <guilima1804@A307-28L.imd.local>
2018-08-02 12:03:21 -0300

    Separação da função

---
+++ raise.cpp
@@ -0,0 +1,21 @@
+
+#include "raise.h"
+
+
+
+unsigned long long calcExpo (int base, int expo){
+	unsigned long long int total(1);
+	for(int i(0); i < expo + 1; i++){
+		
+		if(i == 0){
+			std::cout << base <<" elevado a 0 = 1"<< std::endl; 
+		} 
+		else {
+			total *= base;
+			std::cout << base << " elevado a " << i << " = " << total << std::endl; 
+			
+		}
+	}
+	return total;
+}
+
