44baf2c13ec66f40f90e938fcb30c169fcf4f6d3
Guilherme de Lima Menezes <guilima1804@A307-28L.imd.local>
2018-08-02 11:35:43 -0300

    Atualizado dia 01/08

--- expo.cpp
+++ expo.cpp
@@ -3,27 +3,33 @@
 #define MIN_BASE 0
 #define MAX_BASE 10
 #define MIN_EXPO 0
-#define MAX_EXPO 20
+#define MAX_EXPO 64
 
-void calcExpo (int base, int expo){
-	unsigned long long int j = 1;
-	for(int i = 0; i < expo + 1; i++){
+/*!
+	\brief Function that return base powers to expo
+
+*/
+
+unsigned long long calcExpo (int base, int expo){
+	unsigned long long int total(1);
+	for(int i(0); i < expo + 1; i++){
 		
 		if(i == 0){
 			std::cout << base <<" elevado a 0 = 1"<< std::endl; 
 		} 
 		else {
-			j = j * base;
-			std::cout << base << " elevado a " << i << " = " << j << std::endl; 
+			total *= base;
+			std::cout << base << " elevado a " << i << " = " << total << std::endl; 
 			
 		}
 	}
+	return total;
 }
 
 int main(){
 	
-	short int base = 0;
-	short int expo = 0;
+	short int base(0);
+	short int expo(0);
 	
 
 	std::cout << "Entre um valor para base entre [" << MIN_BASE << ";" << MAX_BASE << "]: " << std::endl;
@@ -36,8 +42,9 @@ int main(){
 		std::cout << "Entrada invalida. Tente novamente" << std::endl;
 		return 0;
 	} 	
-	
-	calcExpo(base, expo);
+	unsigned long long int resultado = calcExpo(base, expo);
+	std::cout << "O resultado de " << base << " elevado a "
+	<< expo << " é igual a: " << resultado << std::endl;
 	
 
 	return 0;
