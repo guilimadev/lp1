29d191854782365ca675b19f01c92b8201280a06
guilimadev <guilimadev@gmail.com>
2018-08-10 00:51:32 -0300

    Questão 5

---
+++ menor elemento/menor_elemento.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+
+int main(void){
+
+int V[20];
+int menor = 0;
+int position = 0;
+
+for(auto i(0u); i < 20; ++i){
+	V[i] = rand() % 100;
+
+	if(i == 0){
+		menor = V[i];
+		position = i;
+	}
+	else{
+		if(V[i] < menor){
+			menor = V[i];
+			position = i;
+		}
+	}
+	
+
+	std::cout << V[i] << " ";
+}
+
+std::cout << std::endl;
+std::cout << "Menor valor: " << menor << std::endl;
+std::cout << "Se encontra na posição: " << position << std::endl;
+return 0;	
+}
